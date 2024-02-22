47faec761d4a57a415316fc4ea527f8c9fe52c89
IamSadB01 <sergioalfonso.alu@iespacomolla.es>
2024-02-22 15:36:35 +0100

    🚧 Work in progress: finished footer style without making it responsive 🚧

--- application/src/components/Footer/Footer.jsx
+++ application/src/components/Footer/Footer.jsx
@@ -1,48 +1,78 @@
-import React from "react";
+import { Fragment } from "react";
 import "./Footer.css";
-import podProseLogo from "../../assets/PodProse_Logo_Green.svg";
+import PodProseLogo from "../svg/PodProseLogo.jsx";
+import InstagramLogo from "../svg/InstagramLogo.jsx";
+import XLogo from "../svg/XLogo.jsx";
+import GithubLogo from "../svg/GithubLogo.jsx";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div className="container-footer-title">
-          <img src={podProseLogo} alt="PodProse Logo" />
-          <h2>PodProse</h2>
+    <Fragment>
+      <hr className="footer-greenline" />
+      <footer>
+        <div className="footer-cols">
+          <div className="footer-col">
+            <div className="container-footer-col-title">
+              <PodProseLogo />
+              <h4 className="footer-col-title">PodProse</h4>
+            </div>
+            <p className="footer-text">
+              PodProse es narrativa pura. Cada episodio, una nueva aventura a
+              descubrir.
+            </p>
+            <a className="footer-link" href="">
+              ¡ DESCUBRE MÁS !
+            </a>
+          </div>
+          <div className="footer-col">
+            <h4 className="footer-col-title">Contacto</h4>
+            <hr className="footer-col-greenline" />
+            <p className="footer-text">
+              Contacta con nosotros y solucionaremos tus problemas y cuestiones
+              lo antes posible!
+            </p>
+            <a className="footer-link" href="">
+              ¡ CONTÁCTANOS !
+            </a>
+          </div>
+          <div className="footer-col">
+            <h4 className="footer-col-title">Newsletter</h4>
+            <hr className="footer-col-greenline" />
+            <p className="footer-text">
+              Suscríbete gratuitamente a nuestro Newsletter para tener toda la
+              información de tus creadores favoritos.
+            </p>
+            <a className="footer-link" href="">
+              ¡ SUSCRIBETE !
+            </a>
+          </div>
+          <div className="footer-col">
+            <h4 className="footer-col-title">Redes Sociales</h4>
+            <hr className="footer-col-greenline" />
+            <p className="footer-text">
+              Síguenos en todas nuestras redes sociales para estar al tanto de
+              las nuevas actualizaciones!
+            </p>
+            <div className="footer-icons">
+              <InstagramLogo />
+              <XLogo />
+              <GithubLogo />
+            </div>
+          </div>
         </div>
-        <p className="footer-text">
-          PodProse es narrativa pura. Cada episodio, una nueva aventura a
-          descubrir.
-        </p>
-        <a href="">¡DESCUBRE MÁS!</a>
-      </div>
-      <div>
-        <h2>Contacto</h2>
-        <p className="footer-text">
-          Contacta con nosotros y solucionamos tus problemas y cuestionamos los
-          antes posible!
-        </p>
-        <a href="">¡CONTÁCTANOS!</a>
-      </div>
-      <div>
-        <h2>Newsletter</h2>
-        <p className="footer-text">
-          Suscríbete gratuitamente a nuestro Newsletter para tener toda la
-          información de tus creadores favoritos.
-        </p>
-        <a href="">¡SUSCRIBETE!</a>
-      </div>
-      <div>
-        <h2>Redes Sociales</h2>
-        <p className="footer-text">
-          Síguenos en todas nuestras redes sociales para estar al tanto de las
-          nuevas actualizaciones!
-        </p>
-        <a href="">
-          <p>LOGITOS</p>
-        </a>
-      </div>
-    </footer>
+        <hr className="footer-separator-greenline" />
+        <small>
+          <p className="footer-copyright">
+            Creado por SSS <em className="middot">&middot;</em> Copyright 2024{" "}
+            <em className="middot">&middot;</em> Todos los derechos reservados
+          </p>
+          <div className="footer-terms">
+            <a href="">Política de Privacidad</a>
+            <a href="">Términos y Condiciones</a>
+          </div>
+        </small>
+      </footer>
+    </Fragment>
   );
 };
 
