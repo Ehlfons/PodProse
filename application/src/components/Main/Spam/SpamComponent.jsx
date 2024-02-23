9fedbdc20c0a0af77a5cf2d89339ca36a7ad26e9
Sergio Sanchez <sergio098890@gmail.com>
2024-02-23 20:43:20 +0100

    :construction: Work in progress: developing spam :construction:

--- application/src/components/Main/Spam/SpamComponent.jsx
+++ application/src/components/Main/Spam/SpamComponent.jsx
@@ -1,11 +1,34 @@
 import { Fragment } from "react";
 import "./SpamComponent.css";
+import InstagramLogo from "../../svg/InstagramLogo.jsx";
+import XLogo from "../../svg/XLogo.jsx";
+import GithubLogo from "../../svg/GithubLogo.jsx";
 
 const SpamComponent = () => {
   return (
-    <Fragment>  
-      
-    </Fragment>    
+    <Fragment>
+      <div id="container-spam">
+        <div id="social-media-spam">
+          <h2 className="titles-spam">Síguenos en nuestras redes:</h2>
+          <div className="social-media-logos-spam">
+            <InstagramLogo />
+            <XLogo />
+            <GithubLogo />
+          </div>
+        </div>
+        <div id="social-newsletter-spam">
+          <h2 className="titles-spam">PodProse Newsletter</h2>
+          <p>Suscríbete gratuitamente para estar al tanto de todo!</p>
+          <input
+            className="input-spam"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Introduce tu correo electrónico"
+          />
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
