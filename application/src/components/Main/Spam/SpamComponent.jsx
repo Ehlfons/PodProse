c1c2b5c55dc3536bd25bde5dd4b4f07f521a0de5
IamSadB01 <sergioalfonso.alu@iespacomolla.es>
2024-02-24 17:11:26 +0100

    🚧 Work in progress: finished spam section style without making it responsive 🚧

--- application/src/components/Main/Spam/SpamComponent.jsx
+++ application/src/components/Main/Spam/SpamComponent.jsx
@@ -1,33 +1,44 @@
 import { Fragment } from "react";
-import "./SpamComponent.css";
 import InstagramLogo from "../../svg/InstagramLogo.jsx";
 import XLogo from "../../svg/XLogo.jsx";
 import GithubLogo from "../../svg/GithubLogo.jsx";
+import "./SpamComponent.css";
 
 const SpamComponent = () => {
   return (
     <Fragment>
-      <div id="container-spam">
-        <div id="social-media-spam">
-          <h2 className="titles-spam">Síguenos en nuestras redes:</h2>
-          <div className="social-media-logos-spam">
-            <InstagramLogo />
-            <XLogo />
-            <GithubLogo />
+      <section id="container-spam">
+        <div className="spam-wrapper">
+          <div id="social-media-spam">
+            <h2 className="titles-spam">Síguenos en nuestras redes</h2>
+            <div className="social-media-logos-spam">
+              <div className="container-instagram-logo-spam">
+                <hr className="spam-logo-greenLine" />
+                <InstagramLogo />
+              </div>
+              <div className="container-x-logo-spam">
+                <hr className="spam-logo-greenLine" />
+                <XLogo />
+              </div>
+              <div className="container-github-logo-spam">
+                <hr className="spam-logo-greenLine" />
+                <GithubLogo />
+              </div>
+            </div>
+          </div>
+          <div id="social-newsletter-spam">
+            <h2 className="titles-spam">PodProse Newsletter</h2>
+            <p>Suscríbete gratuitamente para estar al tanto de todo!</p>
+            <input
+              className="input-spam"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Introduce tu correo electrónico"
+            />
           </div>
         </div>
-        <div id="social-newsletter-spam">
-          <h2 className="titles-spam">PodProse Newsletter</h2>
-          <p>Suscríbete gratuitamente para estar al tanto de todo!</p>
-          <input
-            className="input-spam"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Introduce tu correo electrónico"
-          />
-        </div>
-      </div>
+      </section>
     </Fragment>
   );
 };
