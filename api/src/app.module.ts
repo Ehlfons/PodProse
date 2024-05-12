e5efecb99d0b6e121931721e5f593aaade4e6e6a
Ehlfons <sergiolfns01@gmail.com>
2024-05-12 18:26:02 +0200

    ✨ New feature: login with google ✨

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,10 +6,15 @@ import { ValidateUserModule } from './validate-user/validate-user.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { FailedLoginModule } from './failed-login/failed-login.module';
 import { BruteForceMiddleware } from './middleware/brute-force.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GoogleStrategy } from './google.strategy'
 
 @Module({
   imports: [ 
     MulterModule.register({ dest: './uploads' }), UsersModule, AuthModule, PrismaModule, ValidateUserModule, FailedLoginModule],
+  controllers: [AppController],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
