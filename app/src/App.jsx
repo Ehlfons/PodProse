04a874db4f9d263baf5293cab4d0b895c2f22886
Ehlfons <sergioalfonso.alu@iespacomolla.es>
2024-05-18 19:17:00 +0200

    📚 Documentation: added .env.sample 📚

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import { Toaster } from "sonner";
 import { BrowserRouter } from "react-router-dom";
-import { Header } from "@components/header";
-import Footer from "@components/footer/Footer";
+import { Header } from "@components/Header";
+import Footer from "@components/Footer/Footer";
 import Routes from "@components/routes/Routes";
 import { ProveedorUsuarios, ProveedorPodcasts } from "@contexts/index";
 import "./App.css";
