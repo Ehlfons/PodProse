73b8aa7ddd944c83017754150b08b3d90f48419c
Ehlfons <sergiolfns01@gmail.com>
2024-05-21 23:26:43 +0200

    fix import paths for header and footer components

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,4 +1,4 @@
 export { default as Header } from "./Header.jsx";
-export { default as Logo } from "./Logo/Logo.jsx";
-export { default as Nav } from "./Nav/Nav.jsx";
-export { default as Login } from "./Login/Login.jsx";
+export { default as Logo } from "./logo/Logo.jsx";
+export { default as Nav } from "./nav/Nav.jsx";
+export { default as Login } from "./login/Login.jsx";
