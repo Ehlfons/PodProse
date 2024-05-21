9e5857b5e342ba4a0fd11c24590eb2bf9b8b629f
Sergio Sanchez <sergio098890@gmail.com>
2024-05-21 23:19:19 +0200

    Add navigation component with styling

---
+++ app/src/components/header/nav/Nav.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <Fragment>
+      <div id="nav">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/home">Explorar</Link> {/* esto será un desplegable en el que aparecerá un search para creadores y podcasts, y las categorías (con posibilidad de filtro directo). */}
+            </li>
+            <li>
+              <Link to="/contact">Contacto</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Nav;
