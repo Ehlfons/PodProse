4a823f51c41c26b88d373b859659157c7a090c76
Sergio Sanchez <sergio098890@gmail.com>
2024-05-27 00:36:08 +0200

    Update FontAwesome CSS import to fix alignment issue

--- app/src/pages/contactPage/ContactPage.jsx
+++ app/src/pages/contactPage/ContactPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./ContactPage.css";
-import "../../assets/fontawesome-6.0.0-alpha2/css/all.css";
+import "../../assets/font-awesome/6.0.0-alpha2/css/all.css";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
