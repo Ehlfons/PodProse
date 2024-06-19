df2b3c9559b31f4ce91ce0708091eb6943743da5
Ehlfons <sergiolfns01@gmail.com>
2024-06-19 17:28:34 +0200

    Added things

---
+++ app/src/pages/creatorPage/CreatorPage.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+// import { FileUpload, FileList } from "@components/files";
+import "./CreatorPage.css";
+
+const CreatorPage = () => {
+  return (
+    <Fragment>
+      {/* <main>
+        <FileUpload />
+        <FileList/>
+      </main> */}
+    </Fragment>
+  );
+};
+
+export default CreatorPage;
